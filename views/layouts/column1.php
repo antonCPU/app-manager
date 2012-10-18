a850c4ce61a6bfd743781fddf3cbc81fcd34bad1
anton <anton@anton>
2012-10-18 22:02:27 +0300

    relocated widgets

--- views/layouts/column1.php
+++ views/layouts/column1.php
@@ -10,7 +10,7 @@
 	</div>	
     
     <div id="content">
-        <?php $this->widget('AmFlash'); ?>
+        <?php $this->widget('appManager.components.widgets.AmFlash'); ?>
         <?php echo $content; ?>
     </div>
 <?php $this->endContent(); ?>
