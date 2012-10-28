18a5f17fadbd3b341648a6e0604b4404c65affa9
anton <anton@anton>
2012-10-28 19:11:32 +0200

    added docs

--- AppManagerModule.php
+++ AppManagerModule.php
@@ -1,5 +1,28 @@
 <?php
-        
+/**
+ * AppManager module class file.
+ *
+ * @author Anton Yakushin <yakushinanton@gmail.com>
+ * @version 1.0a
+ * @license BSD
+ */
+/**
+ * App manager provides user interface to the Yii application configue.
+ * 
+ * AppManager provides ability to browse all installed components, modules, extensions,
+ * activate/deactivate them and update their settings. Also the Yii core components
+ * and modules have the same abilities.
+ * 
+ * To activate manager add to Yii configuration 'appManager' under 'modules' section.
+ * For proper work the application configue file (config/main.php) should be 
+ * writable by the server. If it is not, only browsing will be available.
+ * NOTICE: it's recommended to backup your configue file before performing any
+ * actions (except browsing) with AppManager as it will completely override 
+ * the configue during work.
+ * 
+ * @author Anton Yakushin <yakushinanton@gmail.com>
+ * @link https://github.com/antonCPU/app-manager
+ */
 class AppManagerModule extends CWebModule
 {
     public $defaultController = 'app';
