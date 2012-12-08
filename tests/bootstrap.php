219125d468a424c8688293c5e9e2d3ee2671285d
anton <anton@anton>
2012-12-08 21:25:36 +0200

    added config tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,11 @@
 <?php
 
+defined('AM_DATA_DIR') or define('AM_DATA_DIR', dirname(__FILE__).'/data');
+
 // change the following paths if necessary
 $yiit=dirname(__FILE__).'/../../../../framework/yiit.php';
-$config=dirname(__FILE__).'/data/config.php';
+$config=AM_DATA_DIR.'/config.php';
 
-define('AM_DATA_DIR', dirname(__FILE__).'/data');
 
 require_once($yiit);
 
--- tests/components/AmConfigTest.php
+++ tests/components/AmConfigTest.php
@@ -3,17 +3,60 @@
 Yii::import('appManager.components.AmConfig');
 
 class AmConfigTest extends CTestCase
-{
-    /**
-     * @expectedException PHPUnit_Framework_Error
-     */
-    public function testNotExistedConfigLoad()
+{   
+    protected $config;
+    protected $file;
+    protected $source;
+    
+    public function setUp()
     {
-        new AmConfig(AM_DATA_DIR . '/not_existed.php');
+        $source = AM_DATA_DIR . '/config.php';
+        $file   = AM_DATA_DIR . '/config_tmp.php';
+        $this->file = @copy($source, $file) ? $file : $source;
+        $this->config = new AmConfig($this->file);
     }
     
-    public function testExistedConfigLoad()
+    public function tearDown()
     {
-        new AmConfig(AM_DATA_DIR . '/config.php');
+        if ($this->file != AM_DATA_DIR . '/config.php') {
+            @unlink($this->file);
+        }
+    }
+    
+    public function testGetLocation()
+    {
+        $this->assertEquals($this->file, $this->config->getLocation());
+    }
+    
+    public function testIsWritable()
+    {
+        $this->assertSame(is_writable($this->file), $this->config->isWritable());
+    }
+    
+    public function testSaveIfNotWritable()
+    {
+        $file = $this->file;
+        if (!@chmod($file, 0400)) {
+            $this->markTestSkipped('Permissions for the config file can\'t be changed.');
+            return;
+        }
+        
+        try {
+            $this->config->save();
+        } catch (CException $e) {
+            chmod($file, 0775);
+            return;
+        } 
+        chmod($file, 0775);
+        $this->fail('Expected exception CException.');
+    }
+    
+    public function testSaveIfWritable()
+    {
+        if (!@chmod($this->file, 0775)) {
+            $this->markTestSkipped('Permissions for the config file can\'t be changed.');
+            return;
+        }
+        $this->assertTrue($this->config->save());
     }
 }
